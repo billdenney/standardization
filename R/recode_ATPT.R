d6a7d84de58c006c485bd741ecf6af61ee2d8208
SBcytel <Swagata.Bhattacharyya@cytel.com>
2023-02-01 16:51:01 +0530

    updated recode_ATPT.R and pk_variables.R- recode_ATPT(): Update to code for pattern “18 TO 24 Hours” and “-18 TO -24 Hours” which takes the larger value for recoding; added ATPT_mod for removal of brackets prior to derivation pk_variables(): Update to code for recoding of pattern “>400” to AVALN=0

--- R/recode_ATPT.R
+++ R/recode_ATPT.R
@@ -8,37 +8,43 @@ recode_ATPT <- function(data) {
 
   recode_ret <-
    data %>%
+    mutate(
+      ATPT_mod = gsub("[(]|[)]", "", ATPT),
+      ATPT_mod = gsub("^\\s+|\\s+$", "", ATPT_mod)
+    ) %>%
     mutate(ATPTN=case_when(
-      str_detect(ATPT, regex("^PRE-DOSE$", ignore_case = T))~"0",
+      str_detect(ATPT_mod, regex("^-[0-9]+ TO -[0-9]+", ignore_case = T))~ gsub("(.+) TO (.+) HOURS.*", "\\2", ATPT_mod), 
+      str_detect(ATPT_mod, regex("^[0-9]+ TO [0-9]+", ignore_case = T))~gsub("(.+) TO (.+) HOURS.*", "\\2", ATPT_mod), 
+      str_detect(ATPT_mod, regex("^PRE-DOSE$|^predose$", ignore_case = T))~"0",
 
-      grepl("^\\d",ATPT) ~str_extract(ATPT,"^\\d+\\.\\d+|\\d+"),
-      grepl("^-\\d",ATPT) ~str_extract(ATPT,"^-\\d+\\.\\d+|-\\d+"),
-      grepl("^\\d$",ATPT) ~str_extract(ATPT,"^\\d+\\.\\d+|\\d+"),
-      grepl("-\\d$",ATPT) ~str_extract(ATPT,"^-\\d+\\.\\d+|-\\d+"),# day -1; dosing of sgn-liv1 day -1
-      grepl("^Day\\s(\\d)",ATPT,ignore.case = TRUE) ~str_extract(ATPT,"^\\d+\\.\\d+|\\d+"),
-      grepl("^Day\\s(-\\d)",ATPT,ignore.case = TRUE) ~str_extract(ATPT,"^-\\d+\\.\\d+|-\\d+"),
-
-
-      grepl("(\\d\\s)MINS|(\\d\\s)MINUTES|(\\d\\s)HOUR|(\\d\\s)HOURS|(\\d\\s)HRS",ATPT,ignore.case = TRUE)
-      & !grepl("[(]",ATPT)~
-        str_extract(ATPT,"^(-\\d+\\.\\d+)[ MINS]|^(-\\d+)[ MINS]|(\\d+)[ MINS]|(\\d+\\.\\d+)[ MINS]"),
+      grepl("^\\d",ATPT_mod) ~str_extract(ATPT_mod,"^\\d+\\.\\d+|\\d+"),
+      grepl("^-\\d",ATPT_mod) ~str_extract(ATPT_mod,"^-\\d+\\.\\d+|-\\d+"),
+      grepl("^\\d$",ATPT_mod) ~str_extract(ATPT_mod,"^\\d+\\.\\d+|\\d+"),
+      grepl("-\\d$",ATPT_mod) ~str_extract(ATPT_mod,"^-\\d+\\.\\d+|-\\d+"),# day -1; dosing of sgn-liv1 day -1
+      grepl("^Day\\s(\\d)",ATPT_mod,ignore.case = TRUE) ~str_extract(ATPT_mod,"^\\d+\\.\\d+|\\d+"),
+      grepl("^Day\\s(-\\d)",ATPT_mod,ignore.case = TRUE) ~str_extract(ATPT_mod,"^-\\d+\\.\\d+|-\\d+"),
 
 
-      grepl("[(](\\d+)[ MINUTES]|[(](\\d+)[ MINS]|[(](\\d+)[ HOURS]|[(](\\d+)[ HRS]",ATPT, ignore.case = TRUE)~substring(str_extract(ATPT,
+      grepl("(\\d\\s)MINS|(\\d\\s)MINUTES|(\\d\\s)HOUR|(\\d\\s)HOURS|(\\d\\s)HRS",ATPT_mod,ignore.case = TRUE)
+      & !grepl("[(]",ATPT_mod)~
+        str_extract(ATPT_mod,"^(-\\d+\\.\\d+)[ MINS]|^(-\\d+)[ MINS]|(\\d+)[ MINS]|(\\d+\\.\\d+)[ MINS]"),
+
+
+      grepl("[(](\\d+)[ MINUTES]|[(](\\d+)[ MINS]|[(](\\d+)[ HOURS]|[(](\\d+)[ HRS]",ATPT_mod, ignore.case = TRUE)~substring(str_extract(ATPT_mod,
                                                                                                                                      "[(](\\d+)[ MINUTES]|[(](\\d+)[ MINS]|[(](\\d+)[ MIN]|
             [(](\\d+)[ minutes]|[(](\\d+)[ mins]|[(](\\d+)[ min]|
             [(](\\d+)[ HOURS]|[(](\\d+)[ HOUR]|[(](\\d+)[ HRS]|[(](\\d+\\.\\d+)[ HR]|
             [(](\\d+)[ hours]|[(](\\d+)[ hour]|[(](\\d+)[ hrs]|[(](\\d+)[ hr]"),2,nchar(.)),
 
       grepl("[(](\\d+\\.\\d+)[ MINUTES]|[(](\\d+\\.\\d+)[ MINS]|[(](\\d+\\.\\d+)[ HOUR]|
-            [(](\\d+\\.\\d+)[ HRS]", ATPT, ignore.case = TRUE)~substring(str_extract(ATPT,
+            [(](\\d+\\.\\d+)[ HRS]", ATPT_mod, ignore.case = TRUE)~substring(str_extract(ATPT_mod,
                                                                                      "[(](\\d+\\.\\d+)[ MINUTES]|[(](\\d+\\.\\d+)[ MINS]|[(](\\d+\\.\\d+)[ MIN]|
             [(](\\d+\\.\\d+)[ minutes]|[(](\\d+\\.\\d+)[ mins]|[(](\\d+\\.\\d+)[ min]|
             [(](\\d+\\.\\d+)[ HOURS]|[(](\\d+\\.\\d+)[ HOUR]|[(](\\d+\\.\\d+)[ HRS]|[(](\\d+\\.\\d+)[ HR]|
             [(](\\d+\\.\\d+)[ hours]|[(](\\d+\\.\\d+)[ hour]|[(](\\d+\\.\\d+)[ hrs]|[(](\\d+\\.\\d+)[ hr]"),2,nchar(.)),
 
 
-      grepl("Cycle", ATPT, ignore.case = TRUE)~ ATPT,
+      grepl("Cycle", ATPT_mod, ignore.case = TRUE)~ ATPT_mod,
       TRUE ~NA_character_)) %>%
 
   mutate(ATPTN_ret=case_when(
@@ -55,9 +61,9 @@ recode_ATPT <- function(data) {
   )) %>%
     mutate(ATPTN=round(as.numeric(ATPTN)*ATPTN_ret,3),
            ATPT = na_if(ATPT, "")) %>%
-    select(-ATPTN_ret)
+    select(-ATPTN_ret,-ATPT_mod)
 
   recode_ret
 }
 
-utils::globalVariables(c('ATPTN','ATPTN_ret','ATPT'))
+utils::globalVariables(c('ATPTN','ATPTN_ret','ATPT','ATPT_mod'))
